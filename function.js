ccd75bedfba7eaaa8f2e21c2bbae01b0b0b27d87
Mohammad Aminul Islam <maminul2018@gmail.com>
2021-08-26 21:14:52 +0600

    final commit

---
+++ function.js
@@ -0,0 +1,25 @@
+function add(n1,n2){
+
+    return n1+n2;
+}
+
+
+const add2=function add2(n1,n2){
+    return n1+n2;
+}
+
+
+const add3 = function (n1,n2){
+    return n1+n2;
+}
+
+const add4=(n1,n2)=>(n1+n2)
+
+const sum1=add(10,20);
+
+const sum2=add2(10,20);
+
+const sum3=add3(10,20);
+
+const sum4=add4(10,20);
+console.log(sum1,sum2,sum3,sum4)
